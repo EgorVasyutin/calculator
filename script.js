ceb4b87021acb1f29357cf82542eb0429ff50bb2
Egor <vaspovas@gmail.com>
2022-05-31 10:34:57 +0300

    сверстал историю

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-/*
-* Выровнять верстку - перенести часть кнопок на первый ряд
-* Пофиксить переключение в инженерный режим
-* Добавить обработку скобочек
-* Сверстать блок с историей
-* Добавить переключение в просмотр истории и обратно в калькулятор
-* Убрать цифры в css классах и названиях
-* Избавиться от повторений
-*
+/* TODO:
+* переделать на eval -
+* Выровнять верстку - перенести часть кнопок на первый ряд +
+* Пофиксить переключение в инженерный режим +
+* Добавить обработку скобочек -
+* Сверстать блок с историей +
+* Добавить переключение в просмотр истории и обратно в калькулятор +
+* Убрать цифры в css классах и названиях +
+* Избавиться от повторений +
 * */
 
 
@@ -30,9 +30,9 @@ function clearAll () {
     out.textContent = 0;
 }
 
-document.querySelector(".button__signs--ac").onclick = clearAll;
+document.querySelector(".ac").onclick = clearAll;
 
-document.querySelector('.screen__button').onclick = (event) => {
+document.querySelector('.calc__buttons').onclick = (event) => {
     if(event.target.classList.contains('button__signs--ac'))  return;
     out.textContent = '';
     const key = event.target.textContent;
@@ -126,125 +126,43 @@ document.querySelector('.screen__button').onclick = (event) => {
         }
         finish = true
         out.textContent = result
-        geyHistory(a, b, sing)
     }
 }
-document.querySelector('.screen__button-2').onclick = (event2) => {
-    const key = event2.target.textContent;
-    if (digit.includes(key)) {
-        if (b === '' && sing === ''){
-            a += key;
-            out.textContent = a
-        }
-        else if(a !== '' && b !== '' && finish){
-            b = key
-            finish = false
-            out.textContent = b
-        }else{
-            b += key
-            out.textContent = b
-        }
-        return;
-    }
 
-    if (action.includes(key)) {
-        sing = key
-        out.textContent = sing
-        console.log(sing)
-        return;
-    }
-    if(key === '=') {
-        switch (sing){
-            case '+':  result = (+a) + (+b)
-                break
-            case '-': result = a - b
-                break
-            case 'X': result = a * b
-                break
-            case '/': result = a / b
-                if (b === '0'){
-                    out.textContent = 'Ошибка'
-                    a = ''
-                    b = ''
-                    sing = ''
-                    return;
-                }
-                break
-            case '2nd': result = a * Math.pow(10, 0)
-                break
-            case 'x2': result = Math.pow(a, 2)
-                break
-            case 'x3': result = Math.pow(a, 3)
-                break
-            case 'xy': result = Math.pow(a, b)
-                break
-            case 'ex': result = Math.pow(2.7182818284590452, b)
-                break
-            case '10x': result = Math.pow(10, b)
-                break
-            case '1/x': result = 1 / b
-                break
-            case '2x': result = Math.sqrt(a) * 2
-                break
-            case '3x': result = Math.sqrt(a) * 3
-                break
-            case 'yx': result = Math.sqrt(a) * b
-                break
-            case 'log': result = Math.log10(a)
-                break
-            case 'in':
-                break
-            case 'x!':
-                break
-            case 'sin': result = Math.sin(a)
-                break
-            case 'cos': result = Math.cos(a)
-                break
-            case 'tan': result = Math.tan(a)
-                break
-            case 'e': result = Math.E * a
-                break
-            case 'EE':
-                break
-            case 'rad':
-                break
-            case 'sinh': result = Math.sinh(a)
-                break
-            case 'cosh': result = Math.cosh(a)
-                break
-            case 'tanh': result = Math.tanh(a)
-                break
-            case 'p': result = a * 3.14
-                break
-            case 'rand':
-                break
-        }
-        finish = true
-        out.textContent = result
-        geyHistory(a, b, sing)
-    }
-}
 function calcMore() {
-    const more = document.querySelector('.screen__button-2')
-    const btm = document.querySelector('.button__signs--switch')
+    const more = document.querySelector('.calc__buttons-grid')
+    const btm_mode = document.querySelector('.calc__button--mode')
     const disp = document.querySelector('.calc')
-    const nol = document.querySelector('.button__nul--o')
+    const btms = document.querySelector('.calc__buttons')
 
     if (more.style.display === 'none') {
-        more.style.display = 'block'
-        btm.innerHTML =  'Обычный'
+        more.style.display = 'grid'
+        btm_mode.innerHTML =  'ordinary'
         disp.style.width = '800px'
+        btms.style.display = 'grid'
     } else {
         more.style.display = 'none'
-        btm.innerHTML = 'Инженерный'
+        btm_mode.innerHTML = 'Engineering'
         disp.style.width = '400px'
+        btms.style.display = 'block'
     }
 }
-document.querySelector(".button__signs--switch").onclick = calcMore;
+document.querySelector(".calc__button--mode").onclick = calcMore;
 
-
-function geyHistory(a, b, sing) {
-    eval('a'+'sing'+'b')
+function calcHistory() {
+    const btm = document.querySelector('.calc__buttons-grid--ordinary .calc__button--history')
+    const screen = document.querySelector('.calc__history')
+    const calcScreen = document.querySelector('.calc__screen')
+    if (screen.style.display === 'none') {
+        screen.style.display = 'flex'
+        btm.textContent = 'Calculator'
+        calcScreen.style.display = 'none'
+    }else {
+        screen.style.display = 'none'
+        btm.textContent = 'History'
+        calcScreen.style.display = 'flex'
+    }
 }
 
-document.querySelector(".button__signs--history").onclick = geyHistory;
+document.querySelector('.calc__buttons-grid--ordinary .calc__button--history').onclick = calcHistory
+
