9ed0bb8dec42c0818a4618adb18ea768e24b36ef
Тихомиров Григорий Павлович <gptikhomi1@mts.ru>
2022-06-03 15:21:15 +0300

    Добавил настройку prettier и отформатировал все исходники

--- src/script.js
+++ src/script.js
@@ -1,61 +1,61 @@
 // constants
-const screen = document.querySelector(".calc__screen");
-const historyBody = document.querySelector(".calc-history__body");
+const screen = document.querySelector('.calc__screen')
+const historyBody = document.querySelector('.calc-history__body')
 
 /* state */
-let expression = "";
-const history = [];
+let expression = ''
+const history = []
 
 // logic
 function calculateResult(expression, action) {
   switch (action) {
-    case "2nd":
-      return expression * Math.pow(10, 0);
-    case "x2":
-      return Math.pow(expression, 2);
-    case "x3":
-      return Math.pow(expression, 3);
-    case "xy":
-    case "ex":
-      return Math.pow(Math.E, expression);
-    case "10x":
-      return Math.pow(10, expression);
-    case "1/x":
-      return 1 / expression;
-    case "2x":
-      return Math.sqrt(expression) * 2;
-    case "3x":
-      return Math.sqrt(expression) * 3;
-    case "yx":
-      return Math.sqrt(expression);
-    case "log":
-      return Math.log10(expression);
-    case "in":
-    case "x!":
-    case "sin":
-      return Math.sin(expression);
-    case "cos":
-      return Math.cos(expression);
-    case "tan":
-      return Math.tan(expression);
-    case "e":
-      return Math.E * expression;
-    case "EE":
-    case "rad":
-    case "sinh":
-      return Math.sinh(expression);
-    case "cosh":
-      return Math.cosh(expression);
-    case "tanh":
-      return Math.tanh(expression);
-    case "p":
-      return expression * Math.PI;
-    case "rand":
+    case '2nd':
+      return expression * Math.pow(10, 0)
+    case 'x2':
+      return Math.pow(expression, 2)
+    case 'x3':
+      return Math.pow(expression, 3)
+    case 'xy':
+    case 'ex':
+      return Math.pow(Math.E, expression)
+    case '10x':
+      return Math.pow(10, expression)
+    case '1/x':
+      return 1 / expression
+    case '2x':
+      return Math.sqrt(expression) * 2
+    case '3x':
+      return Math.sqrt(expression) * 3
+    case 'yx':
+      return Math.sqrt(expression)
+    case 'log':
+      return Math.log10(expression)
+    case 'in':
+    case 'x!':
+    case 'sin':
+      return Math.sin(expression)
+    case 'cos':
+      return Math.cos(expression)
+    case 'tan':
+      return Math.tan(expression)
+    case 'e':
+      return Math.E * expression
+    case 'EE':
+    case 'rad':
+    case 'sinh':
+      return Math.sinh(expression)
+    case 'cosh':
+      return Math.cosh(expression)
+    case 'tanh':
+      return Math.tanh(expression)
+    case 'p':
+      return expression * Math.PI
+    case 'rand':
     default:
       try {
-        return eval(expression.slice(0, expression.length - 1));
+        return eval(expression.slice(0, expression.length - 1))
       } catch {
-        return "Некорректный ввод";
+        return 'Некорректный ввод'
       }
   }
 }
@@ -65,70 +65,67 @@ function getHistoryExpressionHTML(expression) {
 }
 
 function onCalcButtonClick(event) {
-  if (!event.target.classList.contains("calc__button")) return;
+  if (!event.target.classList.contains('calc__button')) return
 
-  if (expression.includes("=")) {
-    expression = screen.innerText = "";
+  if (expression.includes('=')) {
+    expression = screen.innerText = ''
 
-    if (event.target.innerText === "=") {
-      return;
+    if (event.target.innerText === '=') {
+      return
     }
   }
-
 
   const buttonClassName = event.target.className
   if (buttonClassName.includes('--clear')) {
-    expression = screen.innerText = "";
-    return;
+    expression = screen.innerText = ''
+    return
   } else if (buttonClassName.includes('--mode')) {
-    return;
+    return
   } else if (buttonClassName.includes('--history')) {
-
-
-    history.forEach((expression) => getHistoryExpressionHTML(expression));
+    history.forEach((expression) => getHistoryExpressionHTML(expression))
     function closeHistory() {
-      historyDisplay.style.display = "none";
+      historyDisplay.style.display = 'none'
     }
-    document.querySelector(".calc-history__close").onclick = closeHistory;
+    document.querySelector('.calc-history__close').onclick = closeHistory
 
-    historyDisplay.style.display = "block";
+    historyDisplay.style.display = 'block'
 
-    return;
+    return
   }
 
-  expression += event.target.innerText;
+  expression += event.target.innerText
   if (!/^[0-9]$/.test(event.target.innerText)) {
-    const action = event.target.innerText;
+    const action = event.target.innerText
 
-    if (event.target.innerText === "=") {
-      const result = calculateResult(expression, action);
-      console.log(result);
-      expression += result;
-      history.push(expression);
-      console.log(history);
+    if (event.target.innerText === '=') {
+      const result = calculateResult(expression, action)
+      console.log(result)
+      expression += result
+      history.push(expression)
+      console.log(history)
     }
   }
 
-  screen.innerText = expression;
+  screen.innerText = expression
 }
-document.addEventListener("click", onCalcButtonClick);
+document.addEventListener('click', onCalcButtonClick)
 
 function calcMore() {
-  const more = document.querySelector(".calc__buttons-grid");
-  const btm_mode = document.querySelector(".calc__button--mode");
-  const disp = document.querySelector(".calc");
-  const btms = document.querySelector(".calc__buttons");
+  const more = document.querySelector('.calc__buttons-grid')
+  const btm_mode = document.querySelector('.calc__button--mode')
+  const disp = document.querySelector('.calc')
+  const btms = document.querySelector('.calc__buttons')
 
-  if (more.style.display === "none") {
-    more.style.display = "grid";
-    btm_mode.innerHTML = "ordinary";
-    disp.style.width = "800px";
-    btms.style.display = "grid";
+  if (more.style.display === 'none') {
+    more.style.display = 'grid'
+    btm_mode.innerHTML = 'ordinary'
+    disp.style.width = '800px'
+    btms.style.display = 'grid'
   } else {
-    more.style.display = "none";
-    btm_mode.innerHTML = "Engineering";
-    disp.style.width = "400px";
-    btms.style.display = "block";
+    more.style.display = 'none'
+    btm_mode.innerHTML = 'Engineering'
+    disp.style.width = '400px'
+    btms.style.display = 'block'
   }
 }
-document.querySelector(".calc__button--mode").onclick = calcMore;
+document.querySelector('.calc__button--mode').onclick = calcMore
